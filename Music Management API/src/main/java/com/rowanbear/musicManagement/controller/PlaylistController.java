96168fb458de8fff7ef6f4b0a38b9bd4b2672bd8
Rowan Bear <rowan@rowans-computer.lan>
2022-03-12 18:37:45 -0700

    third commit

--- Music Management API/src/main/java/com/rowanbear/musicManagement/controller/PlaylistController.java
+++ Music Management API/src/main/java/com/rowanbear/musicManagement/controller/PlaylistController.java
@@ -1,11 +1,11 @@
 package com.rowanbear.musicManagement.controller;
 
+import java.util.ArrayList;
 import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,34 +22,34 @@ import com.rowanbear.musicManagement.service.PlaylistService;
  * @author rowan
  *
  */
-//when does this become plural? Where does the S come from
+
 
 public class PlaylistController {
 	
 	@Autowired
 	private PlaylistService service;
 	
-	//CREATE - POST
-	@RequestMapping(method = RequestMethod.POST)
-	public ResponseEntity<Object> 
-	createPlaylist(@RequestBody Set<Long> songIds, @PathVariable Long websiteId) throws Exception{
-		try {
-			return new ResponseEntity<Object>(service.submitNewPlaylist(songIds, websiteId), HttpStatus.OK);
-		}catch(Exception e) {
-			return new ResponseEntity<Object>(e.getMessage(), HttpStatus.BAD_REQUEST);
-		}
-	}
-	
-	//READ - GET
-	//for specific playlist
-	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
-	public ResponseEntity<Object> getPlaylist(@PathVariable Long id) throws Exception{
-		try{
-			return new ResponseEntity<Object>(service.getPlaylistById(id), HttpStatus.OK);
-		}catch(Exception e) {
-			return new ResponseEntity<Object>(e.getMessage(), HttpStatus.NOT_FOUND);
-		}
-	}
+//	//CREATE - POST
+//	@RequestMapping(method = RequestMethod.POST)
+//	public ResponseEntity<Object> 
+//	createPlaylist(@RequestBody Set<Long> songIds) throws Exception{
+//		try {
+//			return new ResponseEntity<Object>(service.submitNewPlaylist(songIds), HttpStatus.OK);
+//		}catch(Exception e) {
+//			return new ResponseEntity<Object>(e.getMessage(), HttpStatus.BAD_REQUEST);
+//		}
+//	}
+//	
+//	//READ - GET
+//	//for specific playlist
+//	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+//	public ResponseEntity<Object> getPlaylist(@PathVariable Long id) throws Exception{
+//		try{
+//			return new ResponseEntity<Object>(service.getPlaylistById(id), HttpStatus.OK);
+//		}catch(Exception e) {
+//			return new ResponseEntity<Object>(e.getMessage(), HttpStatus.NOT_FOUND);
+//		}
+//	}
 	
 	//READ - GET
 	//for all playlists
@@ -62,28 +62,28 @@ public class PlaylistController {
 		}
 	}
 	
-	
-	//UPDATE - PUT
-	//add a song to playlist
-	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
-	public ResponseEntity<Object> addSong(@PathVariable Long id, @RequestBody Song song){
-		try {
-			return new ResponseEntity<Object>(service.addSong(id, song), HttpStatus.OK);
-		}catch(Exception e) {
-			return new ResponseEntity<Object>(e.getMessage(), HttpStatus.NOT_FOUND);
-		}
-	}
-	
-	
-	
-	//DELETE - DELETE
-	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
-	public ResponseEntity<Object> deletePlaylist(@PathVariable Long id){
-		try {
-			service.deletePlaylist(id);
-			return new ResponseEntity<Object>("Successfully deleted playlist with id: " + id, HttpStatus.OK);
-		}catch(Exception e) {
-			return new ResponseEntity<Object>(e.getMessage(), HttpStatus.NOT_FOUND);
-		}
-	}
+//	
+//	//UPDATE - PUT
+//	//add a song to playlist
+//	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+//	public ResponseEntity<Object> addSong(@PathVariable Long id, @RequestBody Song song){
+//		try {
+//			return new ResponseEntity<Object>(service.addSong(id, song), HttpStatus.OK);
+//		}catch(Exception e) {
+//			return new ResponseEntity<Object>(e.getMessage(), HttpStatus.NOT_FOUND);
+//		}
+//	}
+//	
+//	
+//	
+//	//DELETE - DELETE
+//	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+//	public ResponseEntity<Object> deletePlaylist(@PathVariable Long id){
+//		try {
+//			service.deletePlaylist(id);
+//			return new ResponseEntity<Object>("Successfully deleted playlist with id: " + id, HttpStatus.OK);
+//		}catch(Exception e) {
+//			return new ResponseEntity<Object>(e.getMessage(), HttpStatus.NOT_FOUND);
+//		}
+//	}
 }
--- Music Management API/src/main/java/com/rowanbear/musicManagement/service/PlaylistService.java
+++ Music Management API/src/main/java/com/rowanbear/musicManagement/service/PlaylistService.java
@@ -1,6 +1,7 @@
 package com.rowanbear.musicManagement.service;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Optional;
 import java.util.Set;
@@ -29,69 +30,67 @@ public class PlaylistService {
 	
 	//CREATE
 	
-	public Playlist submitNewPlaylist(Set<Long> songIds, Long websitedId) throws Exception {
-		try {
-			Optional<Website> foundWebsite = websiteRepo.findById(websitedId);
-			Website website = foundWebsite.get();
-			Playlist playlist = initializeNewPlaylist(songIds, website);
-			return repo.save(playlist);
-			
-		}catch(Exception e) {
-			throw new Exception("Unable to submit new playlist.");
-		}
-	}
-
-	public Playlist initializeNewPlaylist(Set<Long> songIds, Website website) {
-		Playlist playlist = new Playlist();
-		playlist.setSongs(convertToSongSet(songRepo.findAllById(songIds)));
-		playlist.setDateCreated(LocalDate.now());
-		playlist.setId(playlist.getId());
-		return playlist;
-	}
-
-	public Set<Song> convertToSongSet(Iterable<Song> iterable) {
-		Set<Song> set = new HashSet<Song>();
-		for(Song song:iterable) {
-			set.add(song);
-		}
-		return set;
-	}
-	
-	//READ - GET
-	//for specific song
-	public Playlist getPlaylistById(Long Id) throws Exception {
-		try {
-			Optional <Playlist> site = repo.findById(Id);
-			Playlist playlist = site.get();
-			return playlist;
-		}catch(Exception e) {
-			throw new Exception("Unable to retrieve website by id");
-		}
-	}
-	
+//	public Playlist submitNewPlaylist(Set<Long> songIds) throws Exception {
+//		try {
+//			Playlist playlist = initializeNewPlaylist(songIds);
+//			return repo.save(playlist);
+//			
+//		}catch(Exception e) {
+//			throw new Exception("Unable to submit new playlist.");
+//		}
+//	}
+//
+//	public Playlist initializeNewPlaylist(Set<Long> songIds) {
+//		Playlist playlist = new Playlist();
+//		playlist.setSongs(convertToSongSet(songRepo.findAllById(songIds)));
+//		playlist.setDateCreated(LocalDate.now());
+//		playlist.setId(playlist.getId());
+//		return playlist;
+//	}
+//
+//	public Set<Song> convertToSongSet(Iterable<Song> iterable) {
+//		Set<Song> set = new HashSet<Song>();
+//		for(Song song : iterable) {
+//			set.add(song);
+//		}
+//		return set;
+//	}
+//	
+//	//READ - GET
+//	//for specific song
+//	public Playlist getPlaylistById(Long Id) throws Exception {
+//		try {
+//			Optional <Playlist> site = repo.findById(Id);
+//			Playlist playlist = site.get();
+//			return playlist;
+//		}catch(Exception e) {
+//			throw new Exception("Unable to retrieve website by id");
+//		}
+//	}
+//	
 	//READ
 	//for all playlists
 	public Iterable<Playlist> getPlaylists(){
 		return repo.findAll();
 	}
-	
-	//UPDATE - add a song to playlist
-	public Playlist addSong(Long id, Song song) {
-		Optional<Playlist> oldPlaylist = repo.findById(id);
-		Playlist newPlaylist = oldPlaylist.get();
-		newPlaylist.getSongs().add(song);
-		return repo.save(newPlaylist);
-		
-	}
-	
-	//DELETE 
-	public void deletePlaylist(Long id) throws Exception {
-		try{
-			repo.deleteById(id);
-		}catch(Exception e) {
-			throw new Exception("Unable to delete customer.");
-		}
-	}
+//	
+//	//UPDATE - add a song to playlist
+//	public Playlist addSong(Long id, Song song) {
+//		Optional<Playlist> oldPlaylist = repo.findById(id);
+//		Playlist newPlaylist = oldPlaylist.get();
+//		newPlaylist.getSongs().add(song);
+//		return repo.save(newPlaylist);
+//		
+//	}
+//	
+//	//DELETE 
+//	public void deletePlaylist(Long id) throws Exception {
+//		try{
+//			repo.deleteById(id);
+//		}catch(Exception e) {
+//			throw new Exception("Unable to delete customer.");
+//		}
+//	}
 		
 	
 	
